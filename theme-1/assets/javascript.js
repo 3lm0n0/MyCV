394f77bc606c452376410e931d2d3fc3420ff5c3
3lm0n0 <pedro.jose.bidoggia@hotmail.com>
2024-05-13 12:52:23 -0300

    languages

--- theme-1/assets/javascript.js
+++ theme-1/assets/javascript.js
@@ -11,3 +11,58 @@ document.addEventListener('DOMContentLoaded', function () {
     checkbox.checked = false;
   }
 });
+
+
+//  Language Picker
+// utility functions
+if(!Util) function Util () {};
+
+Util.addClass = function(el, className) {
+  var classList = className.split(' ');
+  el.classList.add(classList[0]);
+  if (classList.length > 1) Util.addClass(el, classList.slice(1).join(' '));
+};
+
+Util.removeClass = function(el, className) {
+  var classList = className.split(' ');
+  el.classList.remove(classList[0]);
+  if (classList.length > 1) Util.removeClass(el, classList.slice(1).join(' '));
+};
+
+Util.toggleClass = function(el, className, bool) {
+  if(bool) Util.addClass(el, className);
+  else Util.removeClass(el, className);
+};
+
+Util.moveFocus = function (element) {
+  if( !element ) element = document.getElementsByTagName('body')[0];
+  element.focus();
+  if (document.activeElement !== element) {
+    element.setAttribute('tabindex','-1');
+    element.focus();
+  }
+};
+
+Util.getIndexInArray = function(array, el) {
+  return Array.prototype.indexOf.call(array, el);
+};
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var language = document.getElementById("language-selector");
+
+  language.addEventListener('change', function () {
+    console.log(language.value);
+    console.log(window.location.href);
+    url = window.location.href;
+    url = url.split("/resume/")[1];
+    console.log(url);
+    if (language.value == "ES" && (url == "index.html" || url == "theme-1/index.html")) {
+      window.location.href = "./es/index.html";
+    }
+    if (language.value == "EN" && url == "theme-1/es/index.html") {
+      window.location.href = "../index.html";
+    }
+  });
+
+});
