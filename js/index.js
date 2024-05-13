08cf4c841ad96fe4e8b21ae8650ac3befb333b2d
3lm0n0 <pedro.jose.bidoggia@hotmail.com>
2024-05-13 14:13:35 -0300

    js laguages path

--- js/index.js
+++ js/index.js
@@ -17,10 +17,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
   language.addEventListener('change', function () {
     url = window.location.href.split("/MyCV/")[1];
-    if (language.value == "ES" && url == "") {
-      window.location.href = "./es/index.html";
+    if (language.value == "ES" && (url == "" || url == "index.html")) {
+      window.location.href = "./theme-1/es/index.html";
     }
-	if (language.value == "ES" && url == "index.html") {
+	  if (language.value == "ES" && url == "theme-1/index.html") {
       window.location.href = "./theme-1/es/index.html";
     }
     if (language.value == "EN" && url == "theme-1/es/index.html") {
